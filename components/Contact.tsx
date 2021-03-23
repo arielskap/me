0c178766ce2a188736fa82e51ace92fd16d645f7
Ariel Villarreal <arielskap@gmail.com>
2021-03-23 00:48:37 -0300

    Se mejoro algunas cositas y se "termino" contacto

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,51 @@
+import Image from 'next/image'
+import SvgEmail from './svg/SvgEmail'
+import SvgPhone from './svg/SvgPhone'
+
 const Contact = () => {
 	return (
 		<section className='pt-16'>
-			<h2>hola</h2>
+			<h2 className='pb-4 text-4xl text-center'>Contacto</h2>
+			<div className='w-1/2 pb-4 mx-auto'>
+				<div className='relative w-full' style={{ height: '150px' }}>
+					<Image
+						src="/me.jpg" alt="Ariel Villarreal mirando al horizonte"
+						layout="fill"
+						objectFit="cover"
+						objectPosition="right"
+						className='rounded-full'
+						/>
+						<div className='absolute top-0 left-0 w-full h-full border rounded-full border-lightBlue-500' />
+				</div>
+			</div>
+			<div className='space-y-4'>
+				<div>
+					<a href='mailto:arielvillagu@hotmail.com'  target="_blank">
+						<div className='flex items-center'>
+							<SvgEmail className='object-contain w-10 fill-current text-lightBlue-500' />
+							<span className='ml-2'>arielvillagu@hotmail.com</span>
+						</div>
+					</a>
+				</div>
+				<div>
+					<a href='tel:+5491121908368' target="_blank">
+						<div className='flex items-center'>
+							<SvgPhone className='object-contain w-10 fill-current text-lightBlue-500' />
+							<span className='ml-2'>+5491121908368</span>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div className='flex justify-center pt-8'>
+				<div className='relative flex'>
+					<a className='w-full h-full' href='/Ariel Villarreal Curriculum.pdf' target='blank'>
+						<div className='px-4 py-2 border rounded border-lightBlue-500'>
+							<span>Obtener CV</span>
+						</div>
+					</a>
+					<div style={{ zIndex: -10 }} className='absolute w-full h-full border-b border-l rounded border-lightBlue-500 right-1 top-1' />
+				</div>
+			</div>
 		</section>
 	)
 }
--- components/Works.tsx
+++ components/Works.tsx
@@ -1,23 +1,47 @@
+import { useEffect, useRef, useState } from 'react';
 import SwiperCore, { Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react'
 import dataWorks from '../json/works.json'
+import SvgGithub from './svg/SvgGithub';
 
 SwiperCore.use([Autoplay])
 
 const Works = () => {
+	const divSwiper = useRef<HTMLDivElement>(null)
+	const [swiper, setSwiper] = useState<SwiperCore>()
+
+	useEffect(() => {
+		if (divSwiper.current && swiper) {
+			const observer = new IntersectionObserver( ( allElements ) => {
+				const isIntersecting = ( element: IntersectionObserverEntry ) => {
+					return element.isIntersecting
+				}
+				allElements
+					.forEach( ( element ) => {
+						if (isIntersecting(element)) {
+							swiper.autoplay.start()
+						} else {
+							swiper.autoplay.stop()
+						}
+					} )
+			}, { threshold: 1 } )
+			observer.observe(divSwiper.current)
+		}
+	}, [swiper])
+
 	return (
-		<section className='pt-16'>
+		<section className='flex flex-col pt-16'>
 			<h2 className='pb-4 text-4xl text-center'>Trabajos</h2>
-			<div className='px-12'>
-				<div className='relative'>
-					<Swiper autoplay spaceBetween={50}>
+			<div className='flex-grow px-12'>
+				<div ref={divSwiper} className='relative'>
+					<Swiper autoplay spaceBetween={50} onInit={(thisSwiper) => { setSwiper(thisSwiper) }}>
 						{dataWorks.map(({ title, href, img, alt }) => {
 							return (
 								<SwiperSlide key={`slide-works-${title}`}>
 									<div>
 										<a target="_blank" href={href}>
 											<div className='relative p-1 border rounded border-lightBlue-500'>
-												<img className='rounded' src={img} alt={alt}/>
+												<img className='object-contain rounded' src={img} alt={alt}/>
 												<div className='absolute top-0 left-0 z-20 flex items-center justify-center w-full h-full bg-black bg-opacity-75 rounded hover:bg-opacity-25'>
 													<p className='px-2 text-2xl text-center text-blue-300 bg-black rounded bg-opacity-90'>{title}</p>
 												</div>
@@ -30,6 +54,14 @@ const Works = () => {
 					</Swiper>
 					<div className='absolute z-0 w-full h-full border rounded right-4 top-4 border-lightBlue-500' />
 				</div>
+			</div>
+			<div className='flex justify-center pt-8'>
+				<a href='https://github.com/arielskap' target='_blank'>
+					<div className='flex items-center'>
+						<SvgGithub className='object-contain w-10 text-white fill-current' />
+					<span className='ml-2'>github.com/arielskap</span>
+					</div>
+				</a>
 			</div>
 		</section>
 	)
